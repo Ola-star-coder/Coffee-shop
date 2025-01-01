7cbf502197a1b24284b3487f2c89e2be37fa924f
olamide <olamideashaolu318.gmail.com>
2025-01-01 04:05:00 +0100

    commit new codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,26 @@ const showElemOnScroll = function(){
    }
 
 
+   // product filter
+const filterBtns = document.querySelectorAll('[data-filter-btn]');
+const filterBox = document.querySelector('[data-filter]');
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function (){
+lastClickedFilterBtn.classList.remove('active');
+this.classList.add('active');
+lastClickedFilterBtn = this;
+
+filterBox.setAttribute('data-filter', this.dataset.filterBtn)
+}
+
+
 // event
 addEventonElem(navTogglers, 'click', toggleNavbar);
 addEventonElem(navbarLinks, 'click', closeNavbar);
 addEventonElem(window, 'scroll', showElemOnScroll);
+addEventonElem(filterBtns, 'click', filter);
+
+
+
